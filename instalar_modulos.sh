1e990a2ecdd98b78cd39062fc0f2b762f45d0624
MauriceStark <maurice222@outlook.com>
2013-09-07 20:37:24 -0500

    modulos agregados

--- instalar_modulos.sh
+++ instalar_modulos.sh
@@ -2,7 +2,25 @@
 
 echo "Instalando modulos"
 
+cd /srv/www/$1/public_html/sites/all/modules ; wget http://ftp.drupal.org/files/projects/ctools-7.x-1.3.tar.gz
+cd /srv/www/$1/public_html/sites/all/modules ; tar zxvf ctools-7.x-1.3.tar.gz
 
+cd /srv/www/$1/public_html/sites/all/modules ; wget http://ftp.drupal.org/files/projects/views-7.x-3.7.tar.gz
+cd /srv/www/$1/public_html/sites/all/modules ; tar zxvf views-7.x-3.7.tar.gz
+
+cd /srv/www/$1/public_html/sites/all/modules ; wget http://ftp.drupal.org/files/projects/token-7.x-1.5.tar.gz
+cd /srv/www/$1/public_html/sites/all/modules ; tar zxvf token-7.x-1.5.tar.gz
+
+cd /srv/www/$1/public_html/sites/all/modules ; wget http://ftp.drupal.org/files/projects/pathauto-7.x-1.2.tar.gz
+cd /srv/www/$1/public_html/sites/all/modules ; tar zxvf pathauto-7.x-1.2.tar.gz
+
+cd /srv/www/$1/public_html/sites/all/modules ; wget http://ftp.drupal.org/files/projects/entity-7.x-1.1.tar.gz
+cd /srv/www/$1/public_html/sites/all/modules ; tar zxvf entity-7.x-1.1.tar.gz
+
+cd /srv/www/$1/public_html/sites/all/modules ; wget http://ftp.drupal.org/files/projects/entityreference-7.x-1.0.tar.gz
+cd /srv/www/$1/public_html/sites/all/modules ; tar zxvf entityreference-7.x-1.0.tar.gz
+
+cd /srv/www/$1/public_html/sites/all/modules ; rm pathauto-7.x-1.2.tar.gz ctools-7.x-1.3.tar.gz token-7.x-1.5.tar.gz views-7.x-3.7.tar.gz entity-7.x-1.1.tar.gz entityreference-7.x-1.0.tar.gz
 
 echo "Modulos Instalados"
 
