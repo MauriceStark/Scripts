680f000112bb9e3a410443d0ac3ffb986ce762ba
MauriceStark <maurice222@outlook.com>
2013-09-07 20:26:42 -0500

    arreglo en el formato del titulo

--- instalar-modulos.sh
+++ instalar-modulos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash/
 
-echo "==========================================Instalando modulos"
+echo "Instalando modulos"
 
 cd /srv/www/$1/public_html/sites/all/modules ; wget http://ftp.drupal.org/files/projects/views-7.x-3.7.tar.gz
 cd /srv/www/$1/public_html/sites/all/modules ; tar zxvf views-7.x-3.7.tar.gz
@@ -22,8 +22,8 @@ cd /srv/www/$1/public_html/sites/all/modules ; tar zxvf entityreference-7.x-1.0.
 
 cd /srv/www/$1/public_html/sites/all/modules ; rm pathauto-7.x-1.2.tar.gz ctools-7.x-1.3.tar.gz token-7.x-1.5.tar.gz views-7.x-3.7.tar.gz entity-7.x-1.1.tar.gz entityreference-7.x-1.0.tar.gz
 
-echo "==========================================Modulos Instalados"
+echo "Modulos Instalados"
 
-echo "==========================================cambiando permisos a settings.php"
+echo "Cambiando permisos a settings.php"
 
 cd /srv/www/$1/public_html/sites/default ; chmod 755 settings.php
