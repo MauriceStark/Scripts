342b1781d261863d899d2b5716d572c73f061bcf
MauriceStark <maurice222@outlook.com>
2013-09-07 20:39:41 -0500

    espacios agregados

--- instalar_drupal.sh
+++ instalar_drupal.sh
@@ -1,18 +1,23 @@
 #!/bin/bash/
 
 echo "==========================================Actualizando"
+
 apt-get -y update
 apt-get install -y apache2 mysql-server git php5 libapache2-mod-php5 make php5-gd php5-curl php5-dev php5-mysql php5-common php-pear
+
 echo "==========================================Paquetes Instalados"
 
 echo "==========================================Creando la base de datos"
+
 mysqladmin -uroot -p create drupal7
 
 echo "==========================================Creando directorio /srv/www "
+
 cd /srv ; mkdir www 
 cd /srv ; chmod 777 www
 
 echo "==========================================Descargando Drupal"
+
 cd /srv/www ; mkdir $1
 cd /srv/www/$1 ; wget http://ftp.drupal.org/files/projects/drupal-7.22.tar.gz
 cd /srv/www/$1 ; tar zxvf drupal-7.22.tar.gz
@@ -22,12 +27,14 @@ cd /srv/www/$1 ; mkdir logs
 cd /srv/www/$1 ; chmod 777 logs
 
 echo "==========================================Creando settings.php"
+
 cd /srv/www/$1/public_html/sites/default ; cp default.settings.php settings.php
 cd /srv/www/$1/public_html/sites/default ; chmod 777 settings.php
 cd /srv/www/$1/public_html/sites/default ; mkdir files
 cd /srv/www/$1/public_html/sites/default ; chmod 777 files
 
 echo "==========================================Creando Virtual Host"
+
 cd /etc/apache2/sites-available ; touch $1
 echo "<Virtualhost *:80>
  ServerName" $1"
@@ -42,9 +49,11 @@ echo "<Virtualhost *:80>
 cd /etc/apache2/sites-available ; a2ensite $1
 
 echo "==========================================instalando modulo apache  para url amigables"
+
 a2enmod rewrite
 
 echo "==========================================instalando uploadprogress"
+
 pecl install uploadprogress
 
 touch /etc/php5/conf.d/uploadprogress.ini
